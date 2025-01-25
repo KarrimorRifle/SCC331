745248c3502c9b224f6017499c8204c998d988fb
Charles Dobson <79115124+DobbaWon@users.noreply.github.com>
2025-01-25 09:44:27 +0000

    Utilised the screen for testing, added LED function to user sensors

--- Sensors/Room_Sensor/src/main.cpp
+++ Sensors/Room_Sensor/src/main.cpp
@@ -1,11 +1,22 @@
 #include <BTstackLib.h>
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 /*
  * BLE Broadcast periodically (Every 20 seconds)
  * Monitor Temperature
  * Send details to server if needed
 */
+
+//-- defines OLED screen dimensions ---
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET    -1 // Reset pin # 
+#define SCREEN_ADDRESS 0x3C // OLED I2C address
+
+//creates OLED display object "display"
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const char *ROOM_UUID = "12345678-1234-5678-1234-567812345678";
 #define ROOM_MAJOR_ID 1
@@ -25,7 +36,19 @@ void setup(void) {
   BLEAdvertisement adv = BTstack.getBLEAdvertisement();
   adv.setIBeacon(ROOM_UUID, ROOM_MAJOR_ID, 0, -59); // UUID, MajorID, MinorID, Measured Power
 
-  Serial.println("Setup complete. Waiting to broadcast.");
+  // Initialise the OLED display:
+  if (!display.begin(SSD1306_I2C_ADDRESS, SCREEN_ADDRESS)) {
+    Serial.println("SSD1306 allocation failed");
+    for (;;);
+  }
+  // Update display:
+  display.clearDisplay();
+  display.setTextSize(1);     
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Setup complete.");
+  display.println("Waiting to broadcast...");
+  display.display(); 
 }
 
 void loop(void) {
@@ -37,6 +60,13 @@ void loop(void) {
 
     isAdvertising = true;
     lastActionTime = currentTime; // Reset the timer
+
+    // Update display:
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Status: Broadcasting!");
+    display.println("Advertising BLE signal!");
+    display.display(); 
   }
 
   // If advertising and it's time to stop:
@@ -45,6 +75,13 @@ void loop(void) {
 
     isAdvertising = false;
     lastActionTime = currentTime; // Reset the timer
+
+    // Update display:
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Status: Idle.");
+    display.println("Waiting to broadcast..");
+    display.display();
   }
 
   BTstack.loop();
--- Sensors/User_Luggage_Sensor/src/main.cpp
+++ Sensors/User_Luggage_Sensor/src/main.cpp
@@ -1,5 +1,7 @@
 #include <BTstackLib.h>
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 /*
  * Listen for BLE Broadcasts
@@ -13,6 +15,24 @@
  * Display a colour correspoding to Luggage / User colour
  * Two Picos that have the same colour = User & Luggage Pair 
 */
+
+// Colour definitions:
+#define RED_PIN 2
+#define GREEN_PIN 3
+#define BLUE_PIN 4
+// Change these per sensor:
+#define RED_BRIGHTNESS 255
+#define GREEN_BRIGHTNESS 0
+#define BLUE_BRIGHTNESS 0
+
+//-- defines OLED screen dimensions ---
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET    -1 // Reset pin # 
+#define SCREEN_ADDRESS 0x3C // OLED I2C address
+
+//creates OLED display object "display"
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define SCAN_DURATION 3000
 #define WAIT_DURATION 20000
@@ -46,9 +66,29 @@ void setup(void) {
   BTstack.setup();
   BTstack.setBLEAdvertisementCallback(advertisementCallback);
 
-  Serial.println("Waiting to start scanning...");
+  // Initialise the OLED display:
+  if (!display.begin(SSD1306_I2C_ADDRESS, SCREEN_ADDRESS)) {
+    Serial.println("SSD1306 allocation failed");
+    for (;;);
+  }
+  // Update display:
+  display.clearDisplay();
+  display.setTextSize(1);     
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Setup complete.");
+  display.println("Waiting to start scanning..");
+  display.println("Room ID: " + oldMajorID);
+  display.display(); 
 
-  // TODO setup colour system
+  // Colour System:
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+
+  analogWrite(RED_PIN, RED_BRIGHTNESS);
+  analogWrite(GREEN_PIN, GREEN_BRIGHTNESS);
+  analogWrite(BLUE_PIN, BLUE_BRIGHTNESS);
 }
 
 void loop(void) {
@@ -65,6 +105,14 @@ void loop(void) {
 
     isScanning = true;
     lastActionTime = currentTime;
+
+    // Update display:
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Status: Scanning!");
+    display.println("Scanning for BLE signal!");
+    display.println("Room ID: " + oldMajorID);
+    display.display(); 
   }
 
   // Stop scanning after the scan duration:
@@ -82,6 +130,14 @@ void loop(void) {
     }
     isScanning = false;
     lastActionTime = currentTime;
+
+    // Update display:
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Status: Idle.");
+    display.println("Waiting to scan..");
+    display.println("Room ID: " + oldMajorID);
+    display.display(); 
   }
   BTstack.loop();
 }
