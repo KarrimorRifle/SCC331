f4ca931a0252e6d9a771a70b803c66b2a5dbbf6e
Melii <a.colley@lancaster.ac.uk>
2025-01-27 17:20:13 +0000

    Refactored code for readability and added a boot file which connects to wifi upon boot

--- hardware/mqtt-device/main.py
+++ hardware/mqtt-device/main.py
@@ -1,41 +1,23 @@
-from umqtt.simple import MQTTClient
-from lib.ubeacon.ibeacon import IBeacon
-import network
 import bluetooth
+from lib.ubeacon.ibeacon import IBeacon
 
-beacon = IBeacon(uuid="12345678-1234-5678-1234-567812345678", major=1, minor=1)
+from backend_connection import BackendConnection
 
-ble = bluetooth.BLE()
-ble.active(True)
-ble.gap_advertise(250_000, adv_data=beacon.adv_data, resp_data=beacon.resp_bytes, connectable=False)
+import time
 
-ssid = "grp3"
-password = "eqdf2376"
+# Bluetooth advertising code, commented out but left here for others to see and work with in future
+#beacon = IBeacon(uuid="12345678-1234-5678-1234-567812345678", major=1, minor=1)
 
-mqtt_server = "185.213.2.121"
-mqtt_user = "czW0b0Vb6JToFwfMvl2BsAveeJ6Iwx9L0zJxe8AsWQrgsbj8kO5Vsf8Hur9rfyc7"
-mqtt_password = ""
+#ble = bluetooth.BLE()
+#ble.active(True)
+#ble.gap_advertise(250_000, adv_data=beacon.adv_data, resp_data=beacon.resp_bytes, connectable=False)
 
-client_uid = "1"
-topic_to_publish = "feeds/hardware-data/" + client_uid
+#Create a connection to the backend through MQTT
+backendConnection = BackendConnection()
 
-
-station = network.WLAN(network.STA_IF)
-
-station.active(True)
-station.connect(ssid, password)
-
-while station.isconnected() == False:
-    print("connecting to:")
-    print(ssid)
-    print(password)
-    pass
-
-client = MQTTClient(client_uid, mqtt_server, user = mqtt_user, password = mqtt_password)
-client.connect()
-
-
+#publish "hello world" every 30 seconds, primarily for testing purposes
 while (True):
-    client.publish(topic_to_publish, "hello world!")
+    backendConnection.publishMessage("hello world!")
     print("published")
     print("")
+    time.sleep(30)
