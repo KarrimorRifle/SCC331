d7f17913584515069867c48ea0cc83833b0b360e
Samuel To <samuelto888@yahoo.co.uk>
2025-01-27 15:56:47 +0000

    fix up init file

--- back-end/mysql/init.sql
+++ back-end/mysql/init.sql
@@ -3,35 +3,6 @@ CREATE DATABASE IF NOT EXISTS accounts;
 CREATE DATABASE IF NOT EXISTS movement;
 
 -- =============================================
--- Microservice-specific Accounts
--- =============================================
-
--- Account Registration Service (Insert only)
-CREATE USER IF NOT EXISTS 'account_registration'@'%' IDENTIFIED BY 'reg_password';
-GRANT INSERT ON accounts.users TO 'account_registration'@'%';
-ALTER USER 'account_registration'@'%' WITH MAX_USER_CONNECTIONS 1;
-
--- Account Cookie Management Service (Update cookie + Read)
-CREATE USER IF NOT EXISTS 'cookie_manager'@'%' IDENTIFIED BY 'cookie_password';
-GRANT SELECT, UPDATE(cookie, last_login) ON accounts.users TO 'cookie_manager'@'%';
-ALTER USER 'cookie_manager'@'%' WITH MAX_USER_CONNECTIONS 1;
-
--- Data Processing Service (Movement Insert)
-CREATE USER IF NOT EXISTS 'data_processor'@'%' IDENTIFIED BY 'process_password';
-GRANT INSERT ON movement.* TO 'data_processor'@'%';
-ALTER USER 'data_processor'@'%' WITH MAX_USER_CONNECTIONS 1;
-
--- Data Reading Service (Movement Read)
-CREATE USER IF NOT EXISTS 'data_reader'@'%' IDENTIFIED BY 'read_password';
-GRANT SELECT ON movement.* TO 'data_reader'@'%';
-ALTER USER 'data_reader'@'%' WITH MAX_USER_CONNECTIONS 1;
-
--- Data Deletion Service (Movement Delete + Read)
-CREATE USER IF NOT EXISTS 'data_admin'@'%' IDENTIFIED BY 'admin_password';
-GRANT SELECT, DELETE ON movement.* TO 'data_admin'@'%';
-ALTER USER 'data_admin'@'%' WITH MAX_USER_CONNECTIONS 1;
-
--- =============================================
 -- Table Structure
 -- =============================================
 USE accounts;
@@ -48,6 +19,42 @@ CREATE TABLE IF NOT EXISTS users (
   INDEX idx_email (email),
   INDEX idx_cookie (cookie)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- =============================================
+-- Microservice-specific Accounts
+-- =============================================
+
+-- Account Registration Service (Insert only)
+CREATE USER IF NOT EXISTS 'account_registration'@'%' IDENTIFIED WITH 'caching_sha2_password' by 'reg_password';
+GRANT INSERT ON accounts.users TO 'account_registration'@'%';
+ALTER USER 'account_registration'@'%' WITH MAX_USER_CONNECTIONS 1;
+FLUSH PRIVILEGES;
+
+
+-- Account Cookie Management Service (Update cookie + Read)
+CREATE USER IF NOT EXISTS 'cookie_manager'@'%' IDENTIFIED WITH 'caching_sha2_password' by 'cookie_password';
+GRANT SELECT, UPDATE(cookie, last_login) ON accounts.users TO 'cookie_manager'@'%';
+ALTER USER 'cookie_manager'@'%' WITH MAX_USER_CONNECTIONS 1;
+FLUSH PRIVILEGES;
+
+-- Data Processing Service (Movement Insert)
+CREATE USER IF NOT EXISTS 'data_processor'@'%' IDENTIFIED WITH 'caching_sha2_password' by 'process_password';
+GRANT INSERT ON movement.* TO 'data_processor'@'%';
+ALTER USER 'data_processor'@'%' WITH MAX_USER_CONNECTIONS 1;
+FLUSH PRIVILEGES;
+
+-- Data Reading Service (Movement Read)
+CREATE USER IF NOT EXISTS 'data_reader'@'%' IDENTIFIED WITH 'caching_sha2_password' by 'read_password';
+GRANT SELECT ON movement.* TO 'data_reader'@'%';
+ALTER USER 'data_reader'@'%' WITH MAX_USER_CONNECTIONS 1;
+FLUSH PRIVILEGES;
+
+-- Data Deletion Service (Movement Delete + Read)
+CREATE USER IF NOT EXISTS 'data_admin'@'%' IDENTIFIED WITH 'caching_sha2_password' by 'admin_password';
+GRANT SELECT, DELETE ON movement.* TO 'data_admin'@'%';
+ALTER USER 'data_admin'@'%' WITH MAX_USER_CONNECTIONS 1;
+FLUSH PRIVILEGES;
+
 
 -- =============================================
 -- Security Hardening
