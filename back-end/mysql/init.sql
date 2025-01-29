06e7def22cc8cf30bc9388b6aaf4c114bc359c50
Samuel To <samuelto888@yahoo.co.uk>
2025-01-29 08:45:24 +0000

    set up tables and env vars

--- back-end/mysql/init.sql
+++ back-end/mysql/init.sql
@@ -1,10 +1,11 @@
 -- Database creation
 CREATE DATABASE IF NOT EXISTS accounts;
-CREATE DATABASE IF NOT EXISTS movement;
+CREATE DATABASE IF NOT EXISTS pico;
 
 -- =============================================
 -- Table Structure
 -- =============================================
+-- Accounts
 USE accounts;
 
 CREATE TABLE IF NOT EXISTS users (
@@ -19,6 +20,29 @@ CREATE TABLE IF NOT EXISTS users (
   INDEX idx_email (email),
   INDEX idx_cookie (cookie)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- pico
+USE pico
+CREATE TABLE IF NOT EXISTS users (
+  picoID INT NOT NULL,
+  roomID INT NOT NULL,
+  logged_at TIMESTAMP NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS luggage ( -- Consider storing it paired up
+  picoID INT NOT NULL,
+  roomID INT NOT NULL,
+  logged_at TIMESTAMP NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS environment (
+  picoID INT NOT NULL,
+  roomID INT NOT NULL,
+  logged_at TIMESTAMP NOT NULL,
+  sound INT NOT NULL,
+  light INT NOT NULL,
+  temperature INT NOT NULL
+)
 
 -- =============================================
 -- Microservice-specific Accounts
@@ -37,21 +61,21 @@ GRANT SELECT, UPDATE(cookie, last_login) ON accounts.users TO 'cookie_manager'@'
 ALTER USER 'cookie_manager'@'%' WITH MAX_USER_CONNECTIONS 1;
 FLUSH PRIVILEGES;
 
--- Data Processing Service (Movement Insert)
+-- Data Processing Service (pico Insert)
 CREATE USER IF NOT EXISTS 'data_processor'@'%' IDENTIFIED WITH 'caching_sha2_password' by 'process_password';
-GRANT INSERT ON movement.* TO 'data_processor'@'%';
+GRANT INSERT ON pico.* TO 'data_processor'@'%';
 ALTER USER 'data_processor'@'%' WITH MAX_USER_CONNECTIONS 1;
 FLUSH PRIVILEGES;
 
--- Data Reading Service (Movement Read)
+-- Data Reading Service (pico Read)
 CREATE USER IF NOT EXISTS 'data_reader'@'%' IDENTIFIED WITH 'caching_sha2_password' by 'read_password';
-GRANT SELECT ON movement.* TO 'data_reader'@'%';
+GRANT SELECT ON pico.* TO 'data_reader'@'%';
 ALTER USER 'data_reader'@'%' WITH MAX_USER_CONNECTIONS 1;
 FLUSH PRIVILEGES;
 
--- Data Deletion Service (Movement Delete + Read)
+-- Data Deletion Service (pico Delete + Read)
 CREATE USER IF NOT EXISTS 'data_admin'@'%' IDENTIFIED WITH 'caching_sha2_password' by 'admin_password';
-GRANT SELECT, DELETE ON movement.* TO 'data_admin'@'%';
+GRANT SELECT, DELETE ON pico.* TO 'data_admin'@'%';
 ALTER USER 'data_admin'@'%' WITH MAX_USER_CONNECTIONS 1;
 FLUSH PRIVILEGES;
 
